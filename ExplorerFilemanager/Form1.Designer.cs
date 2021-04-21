7df4b2a52167a9879aa258e29f632a2e0788287b
守真 孫 <oscarsun72@hotmail.com>
2021-04-21 15:52:27 +0800

    加入專案檔案。

---
+++ ExplorerFilemanager/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace ExplorerFilemanager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(0, 67);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(626, 418);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 18;
+            this.listBox2.Location = new System.Drawing.Point(632, 66);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(414, 418);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.DoubleClick += new System.EventHandler(this.listBox2_DoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(626, 29);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "E:\\!!!!2021@1@所有文檔@\\＃DOCDOCX\\漢字相關\\2漢字doc\\";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(632, 22);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(414, 29);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "F:\\";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1058, 514);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ ExplorerFilemanager/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExplorerFilemanager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        DirectoryInfo di;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listFolders(); listFiles();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        void listFiles()
+        {
+            di = new DirectoryInfo(textBox1.Text);
+            listBox1.DataSource = di.GetFiles();
+        }
+        void listFolders()
+        {
+            di = new DirectoryInfo(textBox2.Text);
+            listBox2.DataSource = di.GetDirectories();
+
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            moveFile();
+            listBox1RefQuery();
+        }
+
+        private void listBox2_DoubleClick(object sender, EventArgs e)
+        {
+            moveFile();
+            listBox1RefQuery();
+
+        }
+
+        private void listBox1RefQuery()
+        {
+            listFiles();
+        }
+
+        void moveFile()
+        {
+            if (listBox1.SelectedItems.Count == 0 || listBox2.SelectedItems.Count == 0) return;
+            FileInfo fi = listBox1.SelectedItem as FileInfo;
+            DirectoryInfo di = listBox2.SelectedItem as DirectoryInfo;
+            string moveToFileFullname = di.FullName + "\\" +
+                 listBox1.SelectedItem.ToString();
+            if (File.Exists(moveToFileFullname))
+            {
+                DialogResult dr = MessageBox.Show("檔案已存在，是否取代原檔案？\r\n" +
+                    "取消作業請按「取消」，\r\n重新命名移動過去的檔，請按「否」。", "注意：", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+                switch (dr)
+                {
+                    case DialogResult.Cancel:
+                        break;
+                    case DialogResult.Yes:
+                        File.Delete(moveToFileFullname);
+                        File.Move(fi.FullName, moveToFileFullname);
+                        break;
+                    case DialogResult.No:
+                        int i = 0;
+                        string ext = fi.Extension;
+                        string movefileName =
+                            moveToFileFullname.Substring(0, moveToFileFullname.IndexOf(ext));
+                        do
+                        {
+                            moveToFileFullname = movefileName
+                                   + (i++.ToString() + ext);
+                        } while (File.Exists(moveToFileFullname));
+                        File.Move(fi.FullName, moveToFileFullname);
+                        break;
+                    default:
+                        break;
+                }
+
+
+            }
+
+        }
+
+        void dialogBoxWarning(string warningMsg)
+        {
+            //DialogResult dr = MessageBox.Show("檔案已存在，是否取代原檔案？", "注意：", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+            DialogResult dr = MessageBox.Show(warningMsg, "注意：", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+            switch (dr)
+            {
+                case DialogResult.Cancel:
+                    //return;
+                    break;
+                case DialogResult.Yes:
+
+                    break;
+                case DialogResult.No:
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
