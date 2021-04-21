c230cddc39c311e2cdd37459734f4b3c78fd64dd
守真 孫 <oscarsun72@hotmail.com>
2021-04-21 16:38:36 +0800

    OK

--- ExplorerFilemanager/Form1.Designer.cs
+++ ExplorerFilemanager/Form1.Designer.cs
@@ -33,27 +33,30 @@ namespace ExplorerFilemanager
             this.listBox2 = new System.Windows.Forms.ListBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 18;
-            this.listBox1.Location = new System.Drawing.Point(0, 67);
+            this.listBox1.Location = new System.Drawing.Point(0, 89);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(626, 418);
             this.listBox1.TabIndex = 0;
             this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            this.listBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDown);
             // 
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
             this.listBox2.ItemHeight = 18;
-            this.listBox2.Location = new System.Drawing.Point(632, 66);
+            this.listBox2.Location = new System.Drawing.Point(632, 88);
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(414, 418);
             this.listBox2.TabIndex = 1;
             this.listBox2.DoubleClick += new System.EventHandler(this.listBox2_DoubleClick);
+            this.listBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listBox2_MouseDown);
             // 
             // textBox1
             // 
@@ -62,6 +65,8 @@ namespace ExplorerFilemanager
             this.textBox1.Size = new System.Drawing.Size(626, 29);
             this.textBox1.TabIndex = 2;
             this.textBox1.Text = "E:\\!!!!2021@1@所有文檔@\\＃DOCDOCX\\漢字相關\\2漢字doc\\";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseDown);
             // 
             // textBox2
             // 
@@ -71,19 +76,33 @@ namespace ExplorerFilemanager
             this.textBox2.TabIndex = 3;
             this.textBox2.Text = "F:\\";
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBox2_MouseDown);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(568, 57);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 26);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1058, 514);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
+            this.KeyPreview = true;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -95,6 +114,7 @@ namespace ExplorerFilemanager
         private System.Windows.Forms.ListBox listBox2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- ExplorerFilemanager/Form1.cs
+++ ExplorerFilemanager/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -23,11 +24,12 @@ namespace ExplorerFilemanager
         private void Form1_Load(object sender, EventArgs e)
         {
             listFolders(); listFiles();
+            comboBox1.DataSource = new List<string> { "move to" };
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-
+            listFolders();
         }
 
         void listFiles()
@@ -38,8 +40,23 @@ namespace ExplorerFilemanager
         void listFolders()
         {
             di = new DirectoryInfo(textBox2.Text);
-            listBox2.DataSource = di.GetDirectories();
+            //            listBox2.DataSource = di.GetDirectories();            
+            List<DirectoryInfo> dList=new List<DirectoryInfo>();
+            foreach (DirectoryInfo item in di.GetDirectories())
+            {
+                if (item.Name.IndexOf("System Volume Information") ==-1)
+                    dList.Add(item);
+            }
+            listBox2.DataSource = dList;
 
+            /*
+            //https://dotblogs.com.tw/marcus116/2011/07/10/31423
+            DirectoryInfo di0 = di.GetDirectories()[0];
+            DirectoryInfo[] listbox2DataSource=di.GetDirectories()
+                .Where(val => val != di0).ToArray();//寫成「.Where<DirectoryInfo>」也無效
+            listBox2.DataSource = listbox2DataSource;
+            //移除無效，再研究 : C# 從陣列中取掉一個元素
+            */
         }
 
         private void listBox1_DoubleClick(object sender, EventArgs e)
@@ -52,7 +69,6 @@ namespace ExplorerFilemanager
         {
             moveFile();
             listBox1RefQuery();
-
         }
 
         private void listBox1RefQuery()
@@ -118,6 +134,74 @@ namespace ExplorerFilemanager
                     break;
             }
 
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            listFiles();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Escape:
+                    this.Close();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {//https://docs.microsoft.com/zh-tw/dotnet/desktop/winforms/input-keyboard/how-to-check-modifier-key?view=netdesktop-5.0
+            if ((Control.ModifierKeys & Keys.Control) == Keys.Control)
+                //MessageBox.Show("KeyPress " + Keys.Shift);
+                if (e.KeyChar == 23) this.Close();//w=23
+        }
+
+        void rightKeyMouse(string fileorDir)
+        {
+            Process.Start(fileorDir);
+        }
+
+        private void textBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                rightKeyMouse(textBox1.Text);
+            }
+        }
+
+        private void textBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                rightKeyMouse(textBox2.Text);
+            }
+        }
+
+        private void listBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                FileInfo fi = listBox1.SelectedItem as FileInfo;
+                rightKeyMouse(fi.FullName);
+            }
+        }
+
+        private void listBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                DirectoryInfo di = listBox2.SelectedItem as DirectoryInfo;
+                rightKeyMouse(di.FullName);
+            }
         }
     }
 }
