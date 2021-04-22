1df6caf6194b3a130e63182a056e35fdace6a46b
守真 孫 <oscarsun72@hotmail.com>
2021-04-23 00:59:41 +0800

    OK

---
+++ ExplorerFilemanager/ClipBoardPlus.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Specialized;
+using System.Windows.Forms;
+
+
+namespace ExplorerFilemanager
+{
+    public static class ClipBoardPlus
+    {
+        public static void CopyFiles(string fileFullname)
+        {
+            StringCollection A =
+                    new StringCollection();
+            A.Add(fileFullname);
+            Clipboard.SetFileDropList(A);
+        }
+
+        //https://docs.microsoft.com/zh-tw/dotnet/desktop/winforms/advanced/how-to-add-data-to-the-clipboard?view=netframeworkdesktop-4.8
+        // Demonstrates SetFileDropList, ContainsFileDroList, and GetFileDropList
+        public static System.Collections.Specialized.StringCollection
+            SwapClipboardFileDropList(
+            System.Collections.Specialized.StringCollection replacementList)
+        {
+            System.Collections.Specialized.StringCollection returnList = null;
+            if (Clipboard.ContainsFileDropList())
+            {
+                returnList = Clipboard.GetFileDropList();
+                Clipboard.SetFileDropList(replacementList);
+            }
+            return returnList;
+        }
+
+    }
+}
